3711a1433ffe816750355f8c85d5b97e5471f6dc
Maya <99327010+kmlndskn@users.noreply.github.com>
2022-11-21 05:39:36 +0700

    Home page

--- js/theme.js
+++ js/theme.js
@@ -9,14 +9,54 @@
         $(".action.show-menu").on('click', function () {
             if(!$(".page-header").hasClass("show-nav")) {
                 $(".page-header").addClass('show-nav');
+                $("html").css({
+                    "overflow": "hidden", 
+                    "height": "100%"
+                 });
             }
         });
 
         // Hide
         $(".action.close-menu").on('click', function () {
             $(".page-header").removeClass('show-nav');
+            $("html").css({
+                "overflow": "auto"
+             });
         });
+         
         
     });
-    
+
+    $(document).on('click','.megamenu .mega-menu-link',function(e){
+
+        e.preventDefault();
+        if($(this).parent().hasClass('active')) {
+            $(this).parent().find("> .mega-sub-menu").slideUp('slow');;
+            $(this).parent().removeClass("active");
+
+        } else {
+            $(".megamenu > .megamenu-item.active").find("> .mega-sub-menu").slideUp('slow');;
+            $(".megamenu > .megamenu-item.active").removeClass("active");
+            $(this).parent().find("> .mega-sub-menu").slideDown('slow');;
+            $(this).parent().addClass("active");
+        }
+    }); 
+
+     // Home Page
+
+     $(document).ready(function(){
+        $("#blogs-slide").owlCarousel({
+            items: 1,
+            loop: true,
+            nav: true,
+            dots: true,
+            autoplay: true,
+            autoplayHoverPause: true,
+            onInitialized: function(){
+                $("#blogs-slide").addClass('loaded');
+            },
+        });
+    });
+
+
 })(jQuery);
